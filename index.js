06ede97827763d894683ca158c41d7f8cda8a257
CuauCabrera <cabrera.cuau@gmail.com>
2022-11-21 16:00:19 -0600

    Se anade la funcion batch para realizar el calculo del imc de varios registros de manera simultanea

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-function calculoImc(){
-    let peso = parseInt(prompt('Ingrese su peso en kilogramos'))
-    let talla = parseFloat(prompt('Ingrese su talla en metros'))
-    let resultado = peso/talla**2
-    if(resultado <= 18.5){
-        alert(`Tu IMC es ${resultado.toFixed(0)} y tienes bajo peso`)
-    }else if(resultado >= 18.5 && resultado <= 24.9 ){
-        alert(`Tu IMC es ${resultado.toFixed(0)} y tienes peso normal`)
-    }else if(resultado >= 25 && resultado <= 29.9){
-        alert(`Tu IMC es ${resultado.toFixed(0)} y tienes sobrepeso`)
-    }else if(resultado >= 30){
-        alert(`Tu IMC es ${resultado.toFixed(0)} y tienes obesidad`)
-    }else{
-        alert(`Los valores ingresados no son correctos`)
+// function calculoImc(){
+//     let peso = parseInt(prompt('Ingrese su peso en kilogramos'))
+//     let talla = parseFloat(prompt('Ingrese su talla en metros'))
+//     let resultado = peso/talla**2
+//     if(resultado <= 18.5){
+//         alert(`Tu IMC es ${resultado.toFixed(0)} y tienes bajo peso`)
+//     }else if(resultado >= 18.5 && resultado <= 24.9 ){
+//         alert(`Tu IMC es ${resultado.toFixed(0)} y tienes peso normal`)
+//     }else if(resultado >= 25 && resultado <= 29.9){
+//         alert(`Tu IMC es ${resultado.toFixed(0)} y tienes sobrepeso`)
+//     }else if(resultado >= 30){
+//         alert(`Tu IMC es ${resultado.toFixed(0)} y tienes obesidad`)
+//     }else{
+//         alert(`Los valores ingresados no son correctos`)
+//     }
+// }
+
+//calculoImc()
+
+
+function batchImc(){
+class Paciente {
+    constructor(peso, talla, agregar) {
+        this.peso = parseInt(peso);
+        this.talla = parseFloat(talla);
+        this.agregar = agregar.toUpperCase()
     }
 }
 
-calculoImc()
+const datosPacientes = [];
+const resultados = [];
+datosPacientes.push(new Paciente(prompt('Ingrese su peso en kilogramos'), prompt('Ingrese su talla en metros'), prompt('¿Desea capturar otro paciente? SI/NO')));
+
+for (i = 0; i < datosPacientes.length; i++) {
+    if (datosPacientes[i].agregar == 'SI') {
+        datosPacientes.push(new Paciente(prompt('Ingrese su peso en kilogramos'), prompt('Ingrese su talla en metros'), prompt('¿Desea capturar otro paciente? si/no')));
+    } else if (datosPacientes[i].agregar == 'NO') {
+        for (i in datosPacientes) {
+            let imc = datosPacientes[i].peso / datosPacientes[i].talla ** 2
+            resultados.push(imc.toFixed(0));
+        }
+    }else{
+        alert('Por favor indica "SI" si quieres ingresar más datos ó "NO" si ya has cargado todos los datos y quieres proceder al cálculo de los resultados');
+    }
+}
+alert(` A continuación se muestran los valores de IMC de cada paciente separados por una coma y presentados en el mismo orden en el que fueron ingresados a la calculadora ===> ${resultados}`);
+}
+
+batchImc();
