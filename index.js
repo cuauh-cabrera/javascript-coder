2b10facdb2c12a4b52b14e7b651abace6b61a4dd
CuauCabrera <cabrera.cuau@gmail.com>
2022-12-07 12:40:23 -0600

    Se agrega manipulacion del DOM y almacenamiento de datos de usuario en local storage

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-function batchImc(){
-class Paciente {
-    constructor(peso, talla, agregar) {
-        this.peso = parseInt(peso);
-        this.talla = parseFloat(talla);
-        this.agregar = agregar.toUpperCase()
+const divContenedor = document.getElementById('contenedor');
+const nombrePaciente = document.getElementById('paciente');
+const datosPeso = document.getElementById('peso');
+const datosTalla = document.getElementById('talla');
+const botonEnviar = document.getElementById('enviar');
+const divResultado = document.getElementById('notificacion')
+
+
+botonEnviar.onclick = () => {
+    const resultadoImc = datosPeso.value/datosTalla.value**2
+    const paciente =  {
+        nombre: nombrePaciente.value,
+        peso: datosPeso.value,
+        talla: datosTalla.value,
+        resultado: resultadoImc.toFixed(0)
     }
-}
+    localStorage.setItem('paciente', JSON.stringify(paciente))
 
-const datosPacientes = [];
-const resultados = [];
-datosPacientes.push(new Paciente(prompt('Ingrese su peso en kilogramos'), prompt('Ingrese su talla en metros'), prompt('¿Desea capturar otro paciente? SI/NO')));
-
-for (i = 0; i < datosPacientes.length; i++) {
-    if (datosPacientes[i].agregar == 'SI') {
-        datosPacientes.push(new Paciente(prompt('Ingrese su peso en kilogramos'), prompt('Ingrese su talla en metros'), prompt('¿Desea capturar otro paciente? si/no')));
-    } else if (datosPacientes[i].agregar == 'NO') {
-        for (i in datosPacientes) {
-            let imc = datosPacientes[i].peso / datosPacientes[i].talla ** 2
-            resultados.push(imc.toFixed(0));
-        }
-    }else{
-        alert('Por favor indica "SI" si quieres ingresar más datos ó "NO" si ya has cargado todos los datos y quieres proceder al cálculo de los resultados');
-    }
+    const notifResultado = document.createElement('h3')
+    notifResultado.innerText = `Hola ${paciente.nombre}, tu IMC es ${paciente.resultado}`
+    divResultado.append(notifResultado)
 }
-alert(` A continuación se muestran los valores de IMC de cada paciente separados por una coma y presentados en el mismo orden en el que fueron ingresados a la calculadora ===> ${resultados}`);
-}
-
-batchImc();
